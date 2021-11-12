04ace3ca74f546436f83f297150982c7ba936fb3
winsweba <winsweb@outlook.com>
2021-11-12 13:38:07 -0800

    add to g

---
+++ Life/Assets/Script/PatrolEm.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PatrolEm : MonoBehaviour
+{
+    public float speed;
+    public float destance;
+    private bool movingRight = true;
+    public Transform groungDetection;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector2.right * speed * Time.deltaTime);
+
+        RaycastHit2D groundInfo = Physics2D.Raycast(groungDetection.position, Vector2.down, destance);
+        if(groundInfo.collider == false)
+        {
+            if(movingRight == true)
+            {
+                transform.eulerAngles = new Vector3(0, -180, 0);
+                movingRight = false;
+            }
+            else
+            {
+                transform.eulerAngles = new Vector3(0, 0, 0);
+                movingRight = true;
+            }
+        }
+    }
+}
