79b373660d377626ee40291260fc0518a9549672
winsweba <winsweb@outlook.com>
2021-11-25 11:15:12 -0800

    unity add

--- Life/Assets/Script/PlayerController.cs
+++ Life/Assets/Script/PlayerController.cs
@@ -21,6 +21,56 @@ public class PlayerController : MonoBehaviour
 
     public Transform grondPos;
     // Start is called before the first frame update
+    /* void Start()
+     {
+         extraJumps = extraJumpsValue;
+         rb = GetComponent<Rigidbody2D>();
+     }
+
+     // Update is called once per frame
+     void FixedUpdate()
+     {
+         isGrounded = Physics2D.OverlapCircle(groundCheck.position, checkRadius, whatIsGround);
+         moveInput = Input.GetAxis("Horizontal");
+         rb.velocity = new Vector2(moveInput * speed, rb.velocity.y);
+
+         if(facingRight == false && moveInput > 0)
+         {
+             Flip();
+         }
+         else if(facingRight == true && moveInput < 0)
+         {
+             Flip();
+         }
+     }
+
+     void Update()
+     {
+         if(isGrounded == true)
+         {
+             extraJumps = extraJumpsValue;
+         }
+         if (Input.GetKeyDown(KeyCode.UpArrow) && extraJumps >0)
+         {
+             rb.velocity = Vector2.up * jumpForce;
+             extraJumps--;
+         }
+         else if (Input.GetKeyDown(KeyCode.UpArrow) && extraJumps == 0 && isGrounded == true)
+         {
+             rb.velocity = Vector2.up * jumpForce;
+         }
+     }
+     void Flip()
+     {
+         facingRight = !facingRight;
+         Vector3 Scaler = transform.localScale;
+         Scaler.x *= -1;
+         transform.localScale = Scaler;
+     }*/
+
+
+    public Joystick joystick;
+
     void Start()
     {
         extraJumps = extraJumpsValue;
@@ -31,14 +81,14 @@ public class PlayerController : MonoBehaviour
     void FixedUpdate()
     {
         isGrounded = Physics2D.OverlapCircle(groundCheck.position, checkRadius, whatIsGround);
-        moveInput = Input.GetAxis("Horizontal");
+        moveInput = joystick.Horizontal;
         rb.velocity = new Vector2(moveInput * speed, rb.velocity.y);
 
-        if(facingRight == false && moveInput > 0)
+        if (facingRight == false && moveInput > 0)
         {
             Flip();
         }
-        else if(facingRight == true && moveInput < 0)
+        else if (facingRight == true && moveInput < 0)
         {
             Flip();
         }
@@ -46,16 +96,17 @@ public class PlayerController : MonoBehaviour
 
     void Update()
     {
-        if(isGrounded == true)
+         float jumpInput = joystick.Vertical;
+        if (isGrounded == true)
         {
             extraJumps = extraJumpsValue;
         }
-        if (Input.GetKeyDown(KeyCode.UpArrow) && extraJumps >0)
+        if (jumpInput > 0.5f && extraJumps > 0)
         {
             rb.velocity = Vector2.up * jumpForce;
             extraJumps--;
         }
-        else if (Input.GetKeyDown(KeyCode.UpArrow) && extraJumps == 0 && isGrounded == true)
+        else if (jumpInput > 0.5f && extraJumps == 0 && isGrounded == true)
         {
             rb.velocity = Vector2.up * jumpForce;
         }
